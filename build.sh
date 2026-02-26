899c7a98a754bf45a7dfb042453b1ff1ab19e2f2
Riaru Moda <riarumoda@gmail.com>
2026-02-27 00:08:03 +0700

    script: drop lto and selinux_develop, re-add droidspaces support

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ setup_environment() {
     export DEVICE_DEFCONFIG="arch/arm64/configs/vendor/xiaomi/$DEVICE_DEFCONFIG_IMPORT"
     export COMPILE_MAIN_DEFCONFIG="vendor/$MAIN_DEFCONFIG_IMPORT"
     export COMPILE_DEVICE_DEFCONFIG="vendor/xiaomi/$DEVICE_DEFCONFIG_IMPORT"
-    export COMPILE_FEATURE_DEFCONFIG="vendor/feature/android-12.config vendor/feature/erofs.config vendor/feature/lineageos.config vendor/feature/lmkd.config vendor/feature/lto.config vendor/feature/wireguard.config"
+    export COMPILE_FEATURE_DEFCONFIG="vendor/feature/android-12.config vendor/feature/erofs.config vendor/feature/lineageos.config vendor/feature/lmkd.config vendor/feature/wireguard.config"
     # KernelSU Settings
     if [[ "$KERNELSU_SELECTOR" == "--ksu=KSU_BLXX" ]]; then
         export KSU_SETUP_URI="https://github.com/backslashxx/KernelSU/raw/refs/heads/master/kernel/setup.sh"
@@ -71,8 +71,18 @@ setup_toolchain() {
 add_patches() {
     # Apply general config patches
     echo "Tuning the rest of default configs..."
+    sed -i 's/# CONFIG_PID_NS is not set/CONFIG_PID_NS=y/' $MAIN_DEFCONFIG
     sed -i 's/CONFIG_HZ_100=y/CONFIG_HZ_250=y/' $MAIN_DEFCONFIG
-    echo "CONFIG_SECURITY_SELINUX_DEVELOP=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_POSIX_MQUEUE=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_SYSVIPC=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_CGROUP_DEVICE=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_DEVTMPFS=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_IPC_NS=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_DEVTMPFS_MOUNT=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_FSCACHE=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_FSCACHE_STATS=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_FSCACHE_HISTOGRAM=y" >> $MAIN_DEFCONFIG
+    echo "CONFIG_IOSCHED_BFQ=y" >> $MAIN_DEFCONFIG
     # Apply kernel rename to defconfig
     sed -i 's/CONFIG_LOCALVERSION="-perf"/CONFIG_LOCALVERSION="-perf-neon"/' arch/arm64/configs/vendor/feature/lineageos.config
     # Apply O3 flags into Kernel Makefile
